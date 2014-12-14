0de6cd0900d3e62e7942c7d0401de94242ef7d49
ptoulis <panos.toulis@gmail.com>
2014-12-13 19:45:27 -0500

    Functions to visualize the datasets. This one plots the digits from the MNIST dataset.

---
+++ examples/data_viz.R
@@ -0,0 +1,27 @@
+## Visualize the MNIST dataset.
+#
+
+mnist.plot.digit <- function(mnist.data, id) {
+  # Plots the digit.
+  # 
+  # SYNOPSIS:
+  #   d = read.csv("examples/data/..train.csv")
+  #   mnist.plot.digit(d, 100)
+  #
+  pixels = mnist.data[id, -c(1)]
+  stopifnot(length(pixels) == 28 * 28, all(is.element(pixels, 0:255)))
+  x = c()
+  y = c()
+  colors = c()
+  # Translate data to points and colors.
+  for(point in seq(0, length(pixels)-1)) {
+    j = point %% 28
+    i = (point - j) / 28
+    x = c(x, j)
+    y = c(y, -i)
+    colors = c(colors, gray(1-pixels[point + 1] / 255))
+  }
+  # Plot the digit.
+  plot(x, y, col=colors, pch=16)
+  print(sprintf("Digit is %d", mnist.data[id, 1]))
+}
